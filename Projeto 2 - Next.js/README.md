59ce48a11a59ed695b75736a4e6772ef51eb2549
Matheus toledo <matheus.tererj@hotmail.com>
2022-08-28 21:26:24 -0300

    commit inicial do projeto 2

---
+++ Projeto 2 - Next.js/README.md
@@ -0,0 +1,7 @@
+# Projeto 2 - Next.js
+Esse projeto tem como intuito servir de projeto aprendizado para o React Framework: Next.js.
+
+## Documentação Next.js
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
---
+++ Projeto 2 - Next.js/pages/index.js
@@ -0,0 +1,24 @@
+
+export default function Home({}) {
+
+  // const recepies = data.recepies
+  return (
+    <div className="banana">
+      {/* <h1>{recepies[0].title}</h1> */}
+    </div>
+  );
+}
+
+// export function getStaticProps() {
+//   return {
+//     props: {
+//       data: {
+//         recepies: [
+//           {
+//             title: "Shake de Abacaxi",
+//           },
+//         ],
+//       },
+//     },
+//   };
+// }
